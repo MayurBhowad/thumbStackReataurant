1dfe91a12b88f94f8da95412e520c8196b99cc88
MayurBhowad <mayurbhowad1995@gmail.com>
2020-12-07 03:19:05 +0530

    initial commit

--- config/keys_dev.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    // MongoURI: 'mongodb://localhost:27017/thumbstack'
-    // MongoURI: 'mongodb+srv://mayurB:mayur12345@cluster0.wudce.mongodb.net/RedStore?retryWrites=true&w=majority',
-
-    //IpStore
-    MongoURI: 'mongodb+srv://mayurB:mayur12345@cluster0.lxieb.mongodb.net/thumbStack'
-}
